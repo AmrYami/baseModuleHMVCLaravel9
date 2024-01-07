f3eea03df3e56fcd204522e17f08830fb3286a13
amryami1 <amr.yami1@gmail.com>
2024-01-07 22:33:54 +0200

    updates

--- tests/Feature/users/UsersTest.php
+++ tests/Feature/users/UsersTest.php
@@ -46,4 +46,31 @@ class UsersTest extends TestCase
         $response->assertRedirect(route('users.index'));
         $this->assertDatabaseHas('users', $user);
     }
+    public function test_update_user()
+    {
+        $name = 'test ' . rand(0, 50) . ' update';
+        $user = [
+            'role' => 1,
+            'name' => $name,
+            'user_name' => 'testuser' . rand(0, 50) . ' update',
+            'email' => 'test' . rand(0, 50) . '@admin.com',
+            'mobile' => '01011' . rand(0, 50) . '6241',
+            'password' => 'admin@admin.com',
+            'password_confirmation' => 'admin@admin.com',
+            'type' => 'admin',
+            'code' => uniqid(),
+            'status' => 1,
+            '_method' => 'patch'
+        ];
+        $response = $this->actingAs($this->user)
+            ->json('post', '/users/2', $user, $this->CSRF);
+
+        unset($user['password_confirmation']);
+        unset($user['role']);
+        unset($user['code']);
+        unset($user['password']);
+        unset($user['_method']);
+        $response->assertRedirect(route('users.index'));
+        $this->assertDatabaseHas('users', ['name' => $name]);
+    }
 }
